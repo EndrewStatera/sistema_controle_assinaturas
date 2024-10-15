925269b8e990d86e1b751dd797a7276007e7e861
Endrew <endrewmss@hotmail.com>
2024-10-15 14:59:22 -0300

    mais uma entidade implementada

--- src/main/java/com/fds/sis_controle_assinaturas/model/Pagamento.java
+++ src/main/java/com/fds/sis_controle_assinaturas/model/Pagamento.java
@@ -1,10 +1,12 @@
 package com.fds.sis_controle_assinaturas.model;
 
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.sql.Date;
 
 @Getter
 @Setter
@@ -12,4 +14,17 @@ import lombok.Setter;
 @NoArgsConstructor
 @Table(name = "tb_pagamentos")
 public class Pagamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cd_pagamento")
+    private Long codigo;
+    @Column(name = "pagamento_assinatura")
+    private AssinaturaModel assinatura;
+    @Column(name = "valor_pago")
+    private Float valorPago;
+    @Column(name = "data_pagamento")
+    private Date dataPagamento;
+    @Column(name = "promocao")
+    private String promocao;
+
 }
