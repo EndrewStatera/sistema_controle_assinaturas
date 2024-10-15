c20ceb0e63edaf4fc456484207d6e1918860872f
Endrew <endrewmss@hotmail.com>
2024-10-15 14:35:51 -0300

    modelos implementados

---
+++ src/main/java/com/fds/sis_controle_assinaturas/model/Aplicativo.java
@@ -0,0 +1,26 @@
+package com.fds.sis_controle_assinaturas.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Aplicativo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo")
+    private Long codigo;
+    @Column(name = "nome_app")
+    private String nomeAplicativo;
+    @Column(name= "custo_mensal")
+    private Float custoMensal;
+
+}
---
+++ src/main/java/com/fds/sis_controle_assinaturas/model/Pagamento.java
@@ -0,0 +1,15 @@
+package com.fds.sis_controle_assinaturas.model;
+
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_pagamentos")
+public class Pagamento {
+}
