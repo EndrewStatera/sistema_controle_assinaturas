fd31e77587d923381149f1e5ca5ffd25a35a6479
Endrew <endrewmss@hotmail.com>
2024-10-15 12:32:20 -0300

    First commit

---
+++ src/main/java/com/fds/sis_controle_assinaturas/SisControleAssinaturasApplication.java
@@ -0,0 +1,13 @@
+package com.fds.sis_controle_assinaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisControleAssinaturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisControleAssinaturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fds/sis_controle_assinaturas/SisControleAssinaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fds.sis_controle_assinaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisControleAssinaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
