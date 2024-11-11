b6fde2f9208da21efb581532742b18bc075f5887
EndrewStatera <endrewmss@hotmail.com>
2024-11-11 20:47:56 -0300

    mais algumas coisas

--- src/main/java/com/fds/sis_controle_assinaturas/dominio/service/ServicoAssinatura.java
+++ src/main/java/com/fds/sis_controle_assinaturas/dominio/service/ServicoAssinatura.java
@@ -5,13 +5,12 @@ import com.fds.sis_controle_assinaturas.dominio.model.AplicativoEntity;
 import com.fds.sis_controle_assinaturas.dominio.model.AssinaturaEntity;
 import com.fds.sis_controle_assinaturas.dominio.model.ClienteEntity;
 import com.fds.sis_controle_assinaturas.dominio.model.PagamentoEntity;
+import com.fds.sis_controle_assinaturas.dominio.persistence.IAssinaturaRepository;
 
 public class ServicoAssinatura {
-    public void criaAssinatura(ClienteEntity cliente, AplicativoEntity app, Date data){
-        
+    private IAssinaturaRepository repository;
+    public ServicoAssinatura(IAssinaturaRepository repository){
+        this.repository = repository;
     }
 
-    public void pagaAssinatura(AssinaturaEntity ass, PagamentoEntity pagamento){
-
-    }
 }
