37b1a86190d424363e9331e027dadb083017172f
Endrew Mateus Dos Santos Soares <20204016@facin.portoalegre.pucrsnet.br>
2024-11-18 18:04:33 -0300

    esboço do properties

--- src/main/java/com/fds/sis_controle_assinaturas/aplicacao/casosDeUso/CriarAssinatura.java
+++ src/main/java/com/fds/sis_controle_assinaturas/aplicacao/casosDeUso/CriarAssinatura.java
@@ -1,9 +1,9 @@
 package com.fds.sis_controle_assinaturas.aplicacao.casosDeUso;
 
 import com.fds.sis_controle_assinaturas.aplicacao.dto.AssinaturaDTO;
-import com.fds.sis_controle_assinaturas.dominio.model.AplicativoEntity;
-import com.fds.sis_controle_assinaturas.dominio.model.AssinaturaEntity;
-import com.fds.sis_controle_assinaturas.dominio.model.ClienteEntity;
+import com.fds.sis_controle_assinaturas.dominio.model.AplicativoModel;
+import com.fds.sis_controle_assinaturas.dominio.model.AssinaturaModel;
+import com.fds.sis_controle_assinaturas.dominio.model.ClienteModel;
 import com.fds.sis_controle_assinaturas.dominio.persistence.IAplicativoRepository;
 import com.fds.sis_controle_assinaturas.dominio.persistence.IAssinaturaRepository;
 import com.fds.sis_controle_assinaturas.dominio.persistence.IClienteRepository;
